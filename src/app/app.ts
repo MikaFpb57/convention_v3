65acf44d3814b22986bc2b4d45c594c1733bea5a
MikaFPB <michael.fumery.fpb57@gmail.com>
2025-11-14 15:41:06 +0100

    V1_141125.1

--- src/app/app.ts
+++ src/app/app.ts
@@ -2,10 +2,13 @@ import { Component, signal } from '@angular/core';
 import { OnInit } from '@angular/core';
 import { initFlowbite } from 'flowbite';
 import { RouterOutlet } from '@angular/router';
+import { NavbarComponent } from "./navbar/navbar.component";
+import { FooterComponent } from "./footer/footer.component";
+import { GestionComponent } from "./gestion/gestion.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, NavbarComponent, FooterComponent, GestionComponent],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
